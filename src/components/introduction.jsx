fd98d7c4c7e7fde44d65bb5bb7cb5a429a986160
kevindennyii <kevin.dennyii@gmail.com>
2020-03-01 00:40:21 -0600

    Updated introduction to add section tag for sidebar

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Introduction extends Component {
   render() {
     return (
-      <div className="colorlib-narrow-content">
-        <div className="row">
-          <div className="col-md-6">
-            Thinking of something awesome to display here, but until then...<br />
-            Mighty Mouse is to save the daaaaaaaay!<br />
-            <div className="project" style={{backgroundImage: 'url(images/Mighty-Mouse-768x432.jpg)'}}>
+      <div>
+        <section className="colorlib-experience" data-section="introduction">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6">
+                Thinking of something awesome to display here, but until then...
+                <br />
+                Mighty Mouse is to save the daaaaaaaay!
+                <br />
+                <div
+                  className="project"
+                  style={{
+                    backgroundImage: "url(images/Mighty-Mouse-768x432.jpg)"
+                  }}
+                ></div>
+              </div>
+            </div>
           </div>
-          </div>
-        </div>
+        </section>
       </div>
-    )
+    );
   }
 }
