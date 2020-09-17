63e811f015157086183052e563cdb365e67c7b58
kevindennyii <kevin.dennyii@gmail.com>
2020-09-17 14:44:20 -0500

    update Black Code Collective info in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import bcc from "../images/static1.squarespace.jpg";
-// import emailjs from "emailjs-com";
+import bcc from "../images/bcclog.png";
 
 const Footer = () => {
   return (
     <div>
-      <div style={{ textAlign: "center" }}>
-        {" "}
-        Kevin Denny II &#169; 2020 All Rights Reserved. <br />
-      </div>
-      <div style={{ textAlign: "center", fontSize: "1.2rem" }}>
+      <div className="bccMember">
         Proud member of{" "}
         <a href="https://blackcodecollective.com/">Black Code Collective</a>{" "}
         <img
           src={bcc}
-          width="31px"
-          height="31px"
+          width="5%"
+          height="5%"
           style={{ borderRadius: "6px" }}
           alt="Black Code Collective log with a black fist in between curly braces"
         />
+      </div>
+      <div style={{ textAlign: "center" }}>
+        {" "}
+        Kevin Denny II &#169; 2020 All Rights Reserved. <br />
       </div>
     </div>
   );
