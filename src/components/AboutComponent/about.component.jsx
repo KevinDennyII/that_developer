04d1363e12d0e159079069919afad0e5a475d677
kevindennyii <kevin.dennyii@gmail.com>
2021-01-25 00:45:02 -0600

    - added photos to be used for updated About page

--- src/components/AboutComponent/about.component.jsx
+++ src/components/AboutComponent/about.component.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import blackFamily from '../../images/black-family-on-couch.png';
+import blackManUsingLaptop from '../../images/black-man-computing.png';
+import blackManLearning from '../../images/black-man-learning.png';
+import blackManGraduate from '../../images/black-man-graduate.png';
+import blackManReading from '../../images/black-man-reading.png';
+import blackPeopleDancing from '../../images/black-people-dancing.png';
 
 const AboutComponent = ({ summary }) => (
   <div>
@@ -15,6 +21,48 @@ const AboutComponent = ({ summary }) => (
                   <span className="heading-meta">About Me</span>
                   <h2 className="colorlib-heading">Who Am I?</h2>
                   {summary}
+                  <div>
+                    <img
+                      width="350px"
+                      src={blackFamily}
+                      alt="black family sitting on couch"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      width="350px"
+                      src={blackManLearning}
+                      alt="black man write on white board"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      width="350px"
+                      src={blackManUsingLaptop}
+                      alt="black man programming"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      width="350px"
+                      src={blackManGraduate}
+                      alt="black man graduate"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      width="350px"
+                      src={blackManReading}
+                      alt="black man reading on couch"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      width="350px"
+                      src={blackPeopleDancing}
+                      alt="black people dancing"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
