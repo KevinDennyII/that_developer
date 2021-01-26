afb882abf6f6f6363838573f8c2dfb25ebc1867b
kevindennyii <kevin.dennyii@gmail.com>
2021-01-26 01:15:50 -0600

    - updated About component with images, using CSS to style image and text

--- src/components/AboutComponent/about.component.jsx
+++ src/components/AboutComponent/about.component.jsx
@@ -5,6 +5,7 @@ import blackManLearning from '../../images/black-man-learning.png';
 import blackManGraduate from '../../images/black-man-graduate.png';
 import blackManReading from '../../images/black-man-reading.png';
 import blackPeopleDancing from '../../images/black-people-dancing.png';
+import { container, aboutImg } from './about.module.scss';
 
 const AboutComponent = () => (
   <div>
@@ -20,59 +21,66 @@ const AboutComponent = () => (
                 <div className="about-desc">
                   <span className="heading-meta">About Me</span>
                   <h2 className="colorlib-heading">Who Am I?</h2>
-                  <div>
+                  <div className={container}>
                     <img
+                      className={aboutImg}
                       width="350px"
                       src={blackFamily}
                       alt="black family sitting on couch"
                     />
-                    I am loving husband, VERY proud father of 2
+                    <p>I am loving husband, VERY proud father of 2.</p>
                   </div>
-                  <div>
+                  <div className={container}>
+                    <p>I am a computer scientist graduate of Virginia Tech, class of 2007</p>
                     <img
+                      className={aboutImg}
                       width="350px"
                       src={blackManGraduate}
                       alt="black man graduate"
                     />
-                    I am a computer scientist graduate of Virginia Tech, class of 2007
+
                   </div>
-                  <div>
+                  <div className={container}>
                     <img
+                      className={aboutImg}
                       width="350px"
                       src={blackManUsingLaptop}
                       alt="black man programming"
                     />
-                    I am software and web developer with over 15 years of experience.
+                    <p>I am software and web developer with over 15 years of experience.</p>
                   </div>
-                  <div>
+                  <div className={container}>
+                    <p>I have a passion to continuously learn.</p>
                     <img
+                      className={aboutImg}
                       width="350px"
                       src={blackManLearning}
                       alt="black man write on white board"
                     />
-                    I have a passion to continuously learn.
                   </div>
-                  <div>
+                  <div className={container}>
                     <img
+                      className={aboutImg}
                       width="350px"
                       src={blackManReading}
                       alt="black man reading on couch"
                     />
                     {/* eslint-disable-next-line max-len */}
-                    I love reading all types of books but when it comes to web development.
-                    <br />
-                    I particular have keen interest in JavaScript.
-                    <br />
-                    {/* eslint-disable-next-line max-len */}
-                    I am avid fan of Kyle Simpson and his "You Don't Know JS" series and "Functional-Light JavaScript"
+                    <p>
+                      I love reading all types of books but when it comes to web development.
+                      I particular have keen interest in JavaScript.
+                      I am avid fan of Kyle Simpson and his "You Don't Know JS" series and "Functional-Light JavaScript"
+                    </p>
                   </div>
-                  <div>
+                  <div className={container}>
+                    <p>I am a curator of music; a DJ and hip hop enthusiast</p>
                     <img
+                      className={aboutImg}
                       width="350px"
                       src={blackPeopleDancing}
                       alt="black people dancing"
                     />
-                    I am a curator of music; a DJ and hip hop enthusiast.
+                    .
                   </div>
                 </div>
               </div>
