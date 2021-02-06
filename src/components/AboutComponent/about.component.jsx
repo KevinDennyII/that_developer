e89c791cd71b0577206c3e72e8fccf643cc6c337
kevindennyii <kevin.dennyii@gmail.com>
2021-02-06 12:15:46 -0600

    - add CSS media query for mobile for about page - add classes for use in media queries

--- src/components/AboutComponent/about.component.jsx
+++ src/components/AboutComponent/about.component.jsx
@@ -5,7 +5,9 @@ import blackManLearning from '../../images/black-man-learning.png';
 import blackManGraduate from '../../images/black-man-graduate.png';
 import blackManReading from '../../images/black-man-reading.png';
 import blackPeopleDancing from '../../images/black-people-dancing.png';
-import { container, aboutImg } from './about.module.scss';
+import {
+  staggered, unstaggered, container, aboutImg,
+} from './about.module.scss';
 
 const AboutComponent = () => (
   <div>
@@ -21,57 +23,120 @@ const AboutComponent = () => (
                 <div className="about-desc">
                   <span className="heading-meta">About Me</span>
                   <h2 className="colorlib-heading">Who Am I?</h2>
-                  <div className={container}>
-                    <img
-                      className={aboutImg}
-                      src={blackFamily}
-                      alt="black family sitting on couch"
-                    />
-                    <p>I am loving husband, VERY proud father of 2.</p>
+                  <div className={staggered}>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackFamily}
+                        alt="black family sitting on couch"
+                      />
+                      <p>
+                        I am loving husband.
+                        <br />
+                        A VERY proud father of 2.
+                      </p>
+                    </div>
+                    <div className={container}>
+                      <p>I am a computer scientist graduate of Virginia Tech, class of 2007</p>
+                      <img
+                        className={aboutImg}
+                        src={blackManGraduate}
+                        alt="black man graduate"
+                      />
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackManUsingLaptop}
+                        alt="black man programming"
+                      />
+                      <p>I am software and web developer with over 15 years of experience.</p>
+                    </div>
+                    <div className={container}>
+                      <p>I have a passion to continuously learn.</p>
+                      <img
+                        className={aboutImg}
+                        src={blackManLearning}
+                        alt="black man write on white board"
+                      />
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackManReading}
+                        alt="black man reading on couch"
+                      />
+                      {/* eslint-disable-next-line max-len */}
+                      <p>
+                        {`I love reading all types of books but when it comes to web development
+                        I like book series such as "You Dont Know JS" by Kyle Simpson.`}
+                      </p>
+                    </div>
+                    <div className={container}>
+                      <p>I am a curator of music; a DJ and hip hop enthusiast.</p>
+                      <img
+                        className={aboutImg}
+                        src={blackPeopleDancing}
+                        alt="black people dancing"
+                      />
+                    </div>
                   </div>
-                  <div className={container}>
-                    <p>I am a computer scientist graduate of Virginia Tech, class of 2007</p>
-                    <img
-                      className={aboutImg}
-                      src={blackManGraduate}
-                      alt="black man graduate"
-                    />
-
-                  </div>
-                  <div className={container}>
-                    <img
-                      className={aboutImg}
-                      src={blackManUsingLaptop}
-                      alt="black man programming"
-                    />
-                    <p>I am software and web developer with over 15 years of experience.</p>
-                  </div>
-                  <div className={container}>
-                    <p>I have a passion to continuously learn.</p>
-                    <img
-                      className={aboutImg}
-                      src={blackManLearning}
-                      alt="black man write on white board"
-                    />
-                  </div>
-                  <div className={container}>
-                    <img
-                      className={aboutImg}
-                      src={blackManReading}
-                      alt="black man reading on couch"
-                    />
-                    {/* eslint-disable-next-line max-len */}
-                    <p>
-                      I love reading all types of books but when it comes to web development I like book series such as "You Dont Know JS" by Kyle Simpson.
-                    </p>
-                  </div>
-                  <div className={container}>
-                    <p>I am a curator of music; a DJ and hip hop enthusiast.</p>
-                    <img
-                      className={aboutImg}
-                      src={blackPeopleDancing}
-                      alt="black people dancing"
-                    />
+                  <div className={unstaggered}>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackFamily}
+                        alt="black family sitting on couch"
+                      />
+                      <p>
+                        I am loving husband.
+                        <br />
+                        A VERY proud father of 2.
+                      </p>
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackManGraduate}
+                        alt="black man graduate"
+                      />
+                      <p>I am a computer scientist graduate of Virginia Tech, class of 2007</p>
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackManUsingLaptop}
+                        alt="black man programming"
+                      />
+                      <p>I am software and web developer with over 15 years of experience.</p>
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackManLearning}
+                        alt="black man write on white board"
+                      />
+                      <p>I have a passion to continuously learn.</p>
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackManReading}
+                        alt="black man reading on couch"
+                      />
+                      <p>
+                        {`I love reading all types of books but when it comes to web development
+                        I like book series such as "You Dont Know JS" by Kyle Simpson.`}
+                      </p>
+                    </div>
+                    <div className={container}>
+                      <img
+                        className={aboutImg}
+                        src={blackPeopleDancing}
+                        alt="black people dancing"
+                      />
+                      <p>I am a curator of music; a DJ and hip hop enthusiast.</p>
+                    </div>
                   </div>
                 </div>
               </div>
