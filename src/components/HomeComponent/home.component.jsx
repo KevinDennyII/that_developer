50f879d9be021b12453d257e3118554855e42ba7
kevindennyii <kevin.dennyii@gmail.com>
2022-01-09 01:17:42 -0600

    Home Page and Foote Update Problem Desired to add Spotify Playlist to home page Needed to update copyright year in the footer
    Solution
    Copy and pasted Spotify embed code
    Updated copyright year to 2022

--- src/components/HomeComponent/home.component.jsx
+++ src/components/HomeComponent/home.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { home } from './home.module.scss';
+import { home, music, quote } from './home.module.scss';
 
 const HomeComponent = () => (
   <div className={home}>
-    "When you change your perspective,
-    <br />
-    you tend to hear things differently."
-    <br />
-    - DJ LookUp
-    <br />
+    <div className={quote}>
+      "When you change your perspective,
+      <br />
+      you tend to hear things differently."
+      <br />
+      - DJ LookUp
+    </div>
+    <div className={music}>
+      This is my go to list whenever I need to get in the zone for coding!
+      <br />
+      <iframe
+        title="DJ LookUp's No Lyrics Just Listen Spotify Playlist"
+        src="https://open.spotify.com/embed/playlist/5c4WKwIIouBRNZclEqvM62?utm_source=generator"
+        width="100%"
+        height="380"
+        frameBorder="0"
+        allowFullScreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      />
+    </div>
   </div>
 );
 export default HomeComponent;
