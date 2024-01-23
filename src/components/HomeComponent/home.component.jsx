518debb55158cb514509fa8dc1a0eeca64d1930e
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-01-23 14:29:50 -0600

    added Tidal playlist to home screen

--- src/components/HomeComponent/home.component.jsx
+++ src/components/HomeComponent/home.component.jsx
@@ -20,6 +20,13 @@ const HomeComponent = () => (
         allowFullScreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       />
+      <div
+        style="position: relative; padding-bottom: 100%; height: 0; overflow: hidden; max-width: 100%;">
+        <iframe
+          src="https://embed.tidal.com/playlists/c216b6a4-2bd8-4db0-b96b-6c905a455434?layout=gridify"
+          frameBorder="0" allowFullScreen
+          style="position: absolute; top: 0; left: 0; width: 100%; height: 1px; min-height: 100%; margin: 0 auto;"></iframe>
+      </div>
     </div>
   </div>
 );
