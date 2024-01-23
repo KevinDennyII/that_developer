1688402539a180a95b999225bd162ed2d5b03b53
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-01-23 14:38:14 -0600

    corrected errors for tidal embed

--- src/components/HomeComponent/home.component.jsx
+++ src/components/HomeComponent/home.component.jsx
@@ -16,17 +16,17 @@ const HomeComponent = () => (
         src="https://open.spotify.com/embed/playlist/5c4WKwIIouBRNZclEqvM62?utm_source=generator"
         width="100%"
         height="380"
-        frameBorder="0"
         allowFullScreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       />
     </div>
     <div
-      style="position: relative; padding-bottom: 100%; height: 0; overflow: hidden; max-width: 100%;">
+      className={music}>
       <iframe
+        title="DJ LookUp's No Lyrics Just Listen Tidal Playlist"
         src="https://embed.tidal.com/playlists/c216b6a4-2bd8-4db0-b96b-6c905a455434?layout=gridify"
-        frameBorder="0" allowFullScreen
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 1px; min-height: 100%; margin: 0 auto;"></iframe>
+        allowFullScreen=""
+        ></iframe>
     </div>
   </div>
 );
