28f5eb0952b0b8d2c2432a7258ed3e19b85539b3
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-01-23 15:03:40 -0600

    reverted changes back to just Spotify playlist

--- src/components/HomeComponent/home.component.jsx
+++ src/components/HomeComponent/home.component.jsx
@@ -10,30 +10,16 @@ const HomeComponent = () => (
       <br />
       - DJ LookUp
     </div>
-    <div style={{display: 'flex', flexDirection: 'row'}}>
-    <div style={{flexGrow: 1}} className={music}>
+    <div className={music}>
       <iframe
         title="DJ LookUp's No Lyrics Just Listen Spotify Playlist"
         src="https://open.spotify.com/embed/playlist/5c4WKwIIouBRNZclEqvM62?utm_source=generator"
         width="100%"
         height="380"
         frameBorder="0"
-        allowFullScreen
+        allowFullScreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      />
-    </div>
-    <div style={{flexGrow: 1}}
-      className={music}>
-      <iframe
-        title="DJ LookUp's No Lyrics Just Listen Tidal Playlist"
-        src="https://embed.tidal.com/playlists/c216b6a4-2bd8-4db0-b96b-6c905a455434?layout=gridify"
-        width="100%"
-        height="380"
-        frameBorder="0"
-        allowFullScreen
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        ></iframe>
-    </div>
+      />z
     </div>
   </div>
 );
